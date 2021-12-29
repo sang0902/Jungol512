c240f22846ed0ba676ef1e50ac80c8c35d02d324
Si7A-00 <Si7A-00@Si7A-15>
2021-12-29 10:58:05 +0900

    jungol

---
+++ Frist project/src/Main512.java
@@ -0,0 +1,9 @@
+public class Main512 {
+	public static void main(String[] args) {
+		int x = 49;
+		double y = 0.268300;
+		System.out.printf("%d * %.6f = %.6f", x, y, x * y);
+		//(형식을 지정(문자열))
+		//("%d * %f = %f\n", x, y x * y)예시 
+	}
+}
